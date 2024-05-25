4c731cd96e8b61ba62c748da3f989de75f550850
deysandip301 <deysandip301@gmail.com>
2024-05-26 00:51:57 +0530

    added footer content and design

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,27 @@
+import './Footer.css';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = () => {
     return (
-        <footer>
-            <p>&copy; 2021</p>
+        <footer className="bottom">
+            <div className="footer">
+                <div className="arrow-up">
+                    <div className="arrow"><FontAwesomeIcon icon={faChevronUp} size="5x"/></div>
+                </div>
+                <div className="footer-content">
+                    <p className="copyright">&copy; 2024 Sandip Dey</p>
+                    <ul className="footer-links">
+                        <li><a href="/projects">Projects</a></li>
+                        <li><a href="/skills">Skills</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="credit">
+                Designed and maintained by <a href="https://www.linkedin.com/in/sandipdey301/" target="_blank" rel="noopener noreferrer">Sandip Dey</a>
+            </div>
         </footer>
     )
 }
