99acc56cd5c7b757226603543385ef05654dab68
deysandip301 <deysandip301@gmail.com>
2024-06-06 11:31:22 +0530

    updated the styling of the skill page

--- src/componenets/skills/SkillHero.jsx
+++ src/componenets/skills/SkillHero.jsx
@@ -1,16 +1,31 @@
+import { motion } from 'framer-motion';
 import "./SkillHero.css";
 import TileComponent from "../common/tileComponent/index.js";
 import skillHero from "/src/assets/images/skill_hero.webp";
 
-
 const SkillHero = () => {
     return (
         <div className="skill-hero">
-            < TileComponent header={'Skills'}
-                            text={'I have acquired a variety of skills over the years like C, C++, Java, HTML, CSS, and JavaScript web development and design. I am always eager to learn new technologies and improve my existing skills.'}
-                            subHeader={'Here are some of the skills that I have acquired over the years.'}
-                            img={skillHero}
-                            position={'img-right'}/>
+            <motion.div
+                initial={{x: -500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+            >
+                <TileComponent
+                    header={'Skills'}
+                    text={'I have acquired a variety of skills over the years like C, C++, Java, HTML, CSS, and JavaScript web development and design. I am always eager to learn new technologies and improve my existing skills.'}
+                    subHeader={'Here are some of the skills that I have acquired over the years.'}
+                    position={'img-right'}
+                />
+            </motion.div>
+            <motion.div
+                initial={{x: 500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+                className="skill-hero-image"
+            >
+                <img src={skillHero} alt="Skill Hero"/>
+            </motion.div>
         </div>
     );
 };
