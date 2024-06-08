cd408880f21aabad4ec8865a1e58da76af88bd07
deysandip301 <deysandip301@gmail.com>
2024-06-08 13:55:25 +0530

    updated the about page motion

--- src/componenets/about/AboutHero.jsx
+++ src/componenets/about/AboutHero.jsx
@@ -1,4 +1,5 @@
-import  './AboutHero.css';
+import { motion } from "framer-motion";
+import './AboutHero.css';
 import TileComponent from "../common/tileComponent/index.js";
 import TypeWriter from "../common/typeWriter/TypeWriter.jsx";
 import aboutHeroImage from "/src/assets/images/hero_image.webp";
@@ -6,21 +7,37 @@ import aboutHeroImage from "/src/assets/images/hero_image.webp";
 const AboutHero = () => {
     return (
         <div className="about-hero">
-            <div className="about-tile">
-                <TileComponent className="about-tile"
-                               header={<span>I am <span className={"name-bold"}>Sandip Dey</span></span>}
-                               subHeader={'I am a programmer and a tech enthusiast.'}
-                               text={'I am a software developer and a tech enthusiast. ' +
-                                   'I have a passion for learning new technologies and building innovative solutions.' +
-                                   ' I am always eager to learn new technologies and improve my existing skills.'}
-                               img={aboutHeroImage}
-                               position={'img-right'}
+            <motion.div
+                className="about-tile"
+                initial={{x: -500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+            >
+                <TileComponent
+                    className="about-tile"
+                    header={<span>I am <span className={"name-bold"}>Sandip Dey</span></span>}
+                    subHeader={'I am a programmer and a tech enthusiast.'}
+                    text={'I am a software developer and a tech enthusiast. ' +
+                        'I have a passion for learning new technologies and building innovative solutions.' +
+                        ' I am always eager to learn new technologies and improve my existing skills.'}
+                    img={aboutHeroImage}
+                    position={'img-right'}
                 />
-            </div>
-            <div className="about-typing">
-                < TypeWriter constantText={"I am "} changingText={["Competitive programmer" , "Web developer", "Tech enthusiast", "Explorer"]} typingSpeed={100} deletionSpeed={50}/>
-            </div>
-            </div>
+            </motion.div>
+            <motion.div
+                className="about-typing"
+                initial={{x: 500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+            >
+                <TypeWriter
+                    constantText={"I am "}
+                    changingText={["Competitive programmer" , "Web developer", "Tech enthusiast", "Explorer"]}
+                    typingSpeed={100}
+                    deletionSpeed={50}
+                />
+            </motion.div>
+        </div>
     )
 }
 
