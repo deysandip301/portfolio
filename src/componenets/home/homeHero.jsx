c211da66ac0db56fa99c6ae0ecdaeb53ce804f8a
deysandip301 <deysandip301@gmail.com>
2024-06-08 13:42:25 +0530

    updated the motion style of the home page

--- src/componenets/home/homeHero.jsx
+++ src/componenets/home/homeHero.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import hero_image from '../../assets/images/hero_image.webp';
 import './homeHero.css';
@@ -6,9 +7,21 @@ const HomeHero = () => {
     return (
         <section className="hero">
             <Link to="/">
-                <img src={hero_image} alt="hero_image" className="hero-image" />
+                <motion.img
+                    src={hero_image}
+                    alt="hero_image"
+                    className="hero-image"
+                    initial={{x: 500}}
+                    animate={{x: 0}}
+                    transition={{duration: 0.5}}
+                />
             </Link>
-            <div className="hero-overlay">
+            <motion.div
+                className="hero-overlay"
+                initial={{x: -500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+            >
                 <div className="hero-content">
                     <div className="left">
                         <h1>&lt;coder&gt;</h1>
@@ -19,7 +32,7 @@ const HomeHero = () => {
                         <p>Building innovative solutions, transforming ideas into reality.</p>
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </section>
     )
 }
