c60f1a96e38d7145431348af66a2d174a1456d5b
deysandip301 <deysandip301@gmail.com>
2024-06-09 16:47:57 +0530

    updated the motion styling of project page

--- src/componenets/projects/projectHero.jsx
+++ src/componenets/projects/projectHero.jsx
@@ -1,15 +1,28 @@
+import { motion } from "framer-motion";
 import './projectHero.css';
 import projectCollage from '../../assets/images/project_collage.webp';
 
 const ProjectHero = () => {
     return (
         <div className="project-hero">
-            <div className="project-hero-text">
+            <motion.div
+                className="project-hero-text"
+                initial={{x: -500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+            >
                 <h1 className="project-heading">Projects</h1>
                 <p className="project-hero-intro">Check out some of my latest projects</p>
                 <p className="project-hero-description">I have worked on a variety of projects, ranging from web development to machine learning. I have experience with a variety of technologies, including React, Node.js, and Python. I am always looking for new projects to work on, so feel free to reach out if you have any ideas!</p>
-            </div>
-            <img src={projectCollage} alt="hero-projects" className="projectHero-hero-img" />
+            </motion.div>
+            <motion.img
+                src={projectCollage}
+                alt="hero-projects"
+                className="projectHero-hero-img"
+                initial={{x: 500}}
+                animate={{x: 0}}
+                transition={{duration: 0.5}}
+            />
         </div>
     )
 }
